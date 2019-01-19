972bf0e1f717b52229989144a913ee8257bee113
Shifa Haider <shifa.haider065@gmail.com>
2019-01-19 16:27:48 +0500

    game on cordova

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -6,10 +6,25 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     window.addEventListener("orientationchange", function(e){
         console.log(e);
+        console.log(e.target);
         console.log(screen.orientation.type);
     });
     //console.log(navigator.notification);
     //navigator.notification.prompt('Please enter your name');
     //navigator.notification.beep(2);
-
 }
+function onSuccess(acceleration) {
+
+    console.log('Acceleration X: ' + acceleration.x + '\n' +
+        'Acceleration Y: ' + acceleration.y + '\n' +
+        'Acceleration Z: ' + acceleration.z + '\n' +
+        'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+function onError() {
+    alert('onError!');
+}
+
+var options = { frequency: 1000 };  // Update every 3 seconds
+
+var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
--- www/js/index.js
+++ www/js/index.js
@@ -12,5 +12,19 @@ function onDeviceReady() {
     //console.log(navigator.notification);
     //navigator.notification.prompt('Please enter your name');
     //navigator.notification.beep(2);
-
 }
+function onSuccess(acceleration) {
+
+    console.log('Acceleration X: ' + acceleration.x + '\n' +
+        'Acceleration Y: ' + acceleration.y + '\n' +
+        'Acceleration Z: ' + acceleration.z + '\n' +
+        'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+function onError() {
+    alert('onError!');
+}
+
+var options = { frequency: 1000 };  // Update every 3 seconds
+
+var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
